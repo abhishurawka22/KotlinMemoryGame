b0d24391958837dee23b7860a2aa080f9abd35ae
Abhishu Rawka <abhishurawka22@gmail.com>
2021-04-02 13:59:45 -0700

    Basic Memory Game

---
+++ app/src/main/java/com/abhishurawka22/mymemory/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.abhishurawka22.mymemory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.abhishurawka22.mymemory.models.BoardSize
+import com.abhishurawka22.mymemory.models.MemoryCard
+import com.abhishurawka22.mymemory.models.MemoryGame
+import com.abhishurawka22.mymemory.utils.DEFAULT_ICONS
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
+    private lateinit var memoryGame: MemoryGame
+    private lateinit var adapter: MemoryBoardAdapter
+
+    private var boardSize: BoardSize = BoardSize.HARD
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        memoryGame = MemoryGame(boardSize)
+
+        adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
+            override fun onCardClicked(position: Int) {
+                updateGameWithFlip(position)
+            }
+
+        })
+        rvBoard.adapter = adapter
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    private fun updateGameWithFlip(position: Int) {
+        memoryGame.flipCard(position)
+        adapter.notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/abhishurawka22/mymemory/models/MemoryCard.kt
@@ -0,0 +1,7 @@
+package com.abhishurawka22.mymemory.models
+
+data class MemoryCard(
+        val identifier: Int,
+        var isFaceUp: Boolean = false,
+        var isMatched: Boolean = false
+)
